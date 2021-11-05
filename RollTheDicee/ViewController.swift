7196200e7ecf4ae37b8e0ec3f08ce60cd0290bf2
Nikolai Saganenko <9952728@gmail.com>
2021-11-05 14:54:52 +0300

    Add UI

--- RollTheDicee/ViewController.swift
+++ RollTheDicee/ViewController.swift
@@ -9,11 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    @IBOutlet weak var dice3: UIImageView!
+    @IBOutlet weak var dice4: UIImageView!
+    @IBOutlet weak var dice5: UIImageView!
+    @IBOutlet weak var dice6: UIImageView!
+    @IBOutlet weak var segmentedCtrl: UISegmentedControl!
+    
+    @IBAction func rollButton(_ sender: UIButton) {
+    // rollDices()
     }
+    @IBAction func numberOfDices(_ sender: UISegmentedControl) {
+        let x = segmentedCtrl.selectedSegmentIndex
+        
+        switch x {
+        case 0: dice1.isHidden = true
+            dice2.isHidden = true
+            dice3.isHidden = true
+            dice4.isHidden = true
+            dice5.isHidden = true
+            dice6.isHidden = false
+        case 1: dice1.isHidden = true
+            dice2.isHidden = true
+            dice3.isHidden = true
+            dice4.isHidden = true
+            dice5.isHidden = false
+            dice6.isHidden = false
+        case 2: dice1.isHidden = true
+            dice2.isHidden = true
+            dice3.isHidden = true
+            dice4.isHidden = false
+            dice5.isHidden = false
+            dice6.isHidden = false
+        case 3: dice1.isHidden = true
+            dice2.isHidden = true
+            dice3.isHidden = false
+            dice4.isHidden = false
+            dice5.isHidden = false
+            dice6.isHidden = false
+        case 4: dice1.isHidden = true
+            dice2.isHidden = false
+            dice3.isHidden = false
+            dice4.isHidden = false
+            dice5.isHidden = false
+            dice6.isHidden = false
+        case 5:dice1.isHidden = false
+            dice2.isHidden = false
+            dice3.isHidden = false
+            dice4.isHidden = false
+            dice5.isHidden = false
+            dice6.isHidden = false
+            
+        default:
+            dice1.isHidden = false
+                dice2.isHidden = false
+                dice3.isHidden = false
+                dice4.isHidden = false
+                dice5.isHidden = false
+                dice6.isHidden = false
+        }
+    }
+  
+
 
 
 }
+
 
